0ab2d56ea84f5419eeb2cf3d22c29d18cee09f87
ecbarnes <ecbarnes@d10139.einet.brad.ac.uk>
2018-12-07 15:33:07 +0000

    Prevent player from moving onto blackhole/pulsar using map wrap.

--- src/uk/ac/bradford/spacegame/GameGUI.java
+++ src/uk/ac/bradford/spacegame/GameGUI.java
@@ -3,7 +3,6 @@ package uk.ac.bradford.spacegame;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.*;
 import java.io.File;
